d64ca3f84ba4c3a24fa565c9cb379f26eaa1fd57
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2024-01-11 10:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-};
+function dateDiffInDays(date1, date2) {
+    const millisecondsPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
 
-// Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+    // Convert input dates to UTC milliseconds
+    const utcDate1 = Date.UTC(
+        parseInt(date1.substring(0, 4)),
+        parseInt(date1.substring(5, 7)) - 1,
+        parseInt(date1.substring(8, 10))
+    );
+
+    const utcDate2 = Date.UTC(
+        parseInt(date2.substring(0, 4)),
+        parseInt(date2.substring(5, 7)) - 1,
+        parseInt(date2.substring(8, 10))
+    );
+
+    // Calculate the difference in days
+    const diffInDays = Math.floor((utcDate2 - utcDate1) / millisecondsPerDay);
+
+    return diffInDays;
+}
